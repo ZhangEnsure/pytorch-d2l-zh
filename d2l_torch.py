529c94d206f2f4becf87a5dd4b625d1192df932c
ZhangEnsure <1695313158@qq.com>
2022-08-10 12:39:00 +0800

    1.增加了 data_iter 函数

--- d2l_torch.py
+++ d2l_torch.py
@@ -4,6 +4,7 @@
 # @Software: PyCharm
 
 import torch
+import random
 from d2l import torch as d2l
 
 """
@@ -17,6 +18,7 @@ def synthetic_data(w, b, num_examples):
     """
     构造一个人造数据集，该数据集是一个带有噪声的线性数据集
     y = w*x + b
+    x 中的每一行都包含一个二维数据样本， y 中的每一行都包含一维标签值（一个标量）
     """
     x = torch.normal(0, 1, size=(num_examples, w.shape[0]))
     y = torch.matmul(x, w) + b
@@ -24,4 +26,18 @@ def synthetic_data(w, b, num_examples):
     return x, y
 
 
-
+def data_iter(batch_size, features, labels):
+    """
+    定义一个data_iter函数，接收批量大小、特征矩阵和标签向量作为输入，生成大小为batch_size的小批量
+    在向前传播计算中，我们需要一次取一个批量大小的样本进行计算
+    """
+    # 返回 features 的第一维长度
+    numberExample = len(features)
+    # 需要一个打乱下标的列表 indices
+    indices = list(range(numberExample))
+    random.shuffle(indices)
+    # 从 0 到 num_example 之间一次取出 batch_size 个大小的数据
+    for i in range(0, numberExample, batch_size):
+        # 获得打乱下标后的顺序 batch_size 个下标列表
+        batch_indices = indices[i:min(i + batch_size, numberExample)]
+        yield features[batch_indices], labels[batch_indices]
--- test_d2l_troch.py
+++ test_d2l_troch.py
@@ -6,6 +6,7 @@
 from d2l_torch import *
 import torch
 
+
 def test_synthetic_data():
     true_w = torch.tensor([2, -3.4])
     true_b = 4.2
@@ -14,6 +15,18 @@ def test_synthetic_data():
     d2l.set_figsize()
     d2l.plt.scatter(features[:, 0].detach().numpy(), labels.detach().numpy(), s=1)
     d2l.plt.scatter(features[:, 1].detach().numpy(), labels.detach().numpy(), s=1)
-    d2l.plt.show() # pycharm 专属
+    d2l.plt.show()  # pycharm 专属
+
 
 # test_synthetic_data()
+
+def test_data_iter():
+    true_w = torch.tensor([2, -3.4])
+    true_b = 4.2
+    features, labels = synthetic_data(true_w, true_b, 1000)
+    batch_size = 100
+    # 我们打印第一个迭代对象的 features 的形状进行验证
+    print(next(iter(data_iter(batch_size, features, labels)))[0].shape)
+
+
+test_data_iter()
